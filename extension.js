9901009be8a410b6af85d3b877887dd51fcd7e1b
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2020-01-20 12:42:19 -0800

    extension.js - fix nvm include path

--- extension.js
+++ extension.js
@@ -12,7 +12,6 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "nvmNodeInclude" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
@@ -20,20 +19,20 @@ function activate(context) {
 	let disposable = vscode.commands.registerCommand('extension.nvmNodeInclude', function () {
 		// The code you place here will be executed every time your command is executed
 
-        // just return out best guess as to the include directory for this version
-        // of node. presume NVM_BIN var points to bin if it exists. if not, presume
-        // /usr/local/include/node. no, it's not sophisticated.
-        let includePath;
-        if (process.env.NVM_BIN) {
-            includePath = process.env.NVM_BIN.replace(/\/bin$/, '/include');
-        } else {
-            includePath = '/usr/local/include/node';
-        }
-        return Promise.resolve(includePath);
-		//vscode.window.showInformationMessage('nvmNodeInclude active');
+    // just return a best guess as to the include directory for this version
+    // of node. presume NVM_BIN var points to bin if it exists. if not, presume
+    // /usr/local/include/node. no, it's not sophisticated.
+    let path;
+    if (process.env.NVM_BIN) {
+      path = process.env.NVM_BIN.replace(/\/bin$/, '/include/node');
+    } else {
+      path = '/usr/local/include/node';
+    }
+    vscode.window.showInformationMessage(`nvmNodeInclude active as ${path}`);
+    return Promise.resolve(path);
 	});
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
